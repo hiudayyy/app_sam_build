49468a328bb14da80fa4a857ee56f4f0583c1f43
htc <htc@Mac-mini-M4.local>
2026-05-15 10:14:34 +0700

    deleted

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Workspace\HTC\csam_mobile"
+export "FLUTTER_ROOT=/users/htc/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/htc/dev/app/SamNFT_MB"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=6"
