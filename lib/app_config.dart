1b2734b98a506b44713af15d4b9c7f6022b9b56c
hieu <hieudh1210@gmail.com>
2026-05-27 14:58:22 +0700

    add print2

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 class AppConfig {
   /// Biến kiểm soát việc in log ra console.
   /// Thiết lập thành false khi đóng gói ứng dụng (Release mode).
-  static const bool isPrint = kDebugMode;
+  static const bool isPrint = false; // && kDebugMode;
 
   /// Hàm in log tập trung. Thay thế cho AppConfig.printEx() hoặc debugAppConfig.printEx() mặc định.
   static void printEx(dynamic msg) {
